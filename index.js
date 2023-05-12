a4d75bff4fd085bb53b5be28599528e5330ced66
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-05-12 11:40:48 -0400

    added sample code from files example

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const authentication = require('./authentication');
 const translateDocumentCreate = require('./creates/translate_document.js');
 
+const hydrators = require('./hydrators');
+const newFile = require('./triggers/newFile');
+const uploadFileV10 = require('./creates/uploadFile_v10');
+
+
 module.exports = {
+  // This is just shorthand to reference the installed dependencies you have.
+  // Zapier will need to know these before we can upload.
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
   authentication: authentication,
-  creates: { [translateDocumentCreate.key]: translateDocumentCreate },
+
+  // Any hydrators go here
+  hydrators,
+
+  // If you want your triggers to show up, you better include it here!
+  triggers: {
+    [newFile.key]: newFile,
+  },
+
+  // If you want your creates to show up, you better include it here!
+  creates: {
+    [uploadFileV10.key]: uploadFileV10,
+    [translateDocumentCreate.key]: translateDocumentCreate
+  },
 };
+
